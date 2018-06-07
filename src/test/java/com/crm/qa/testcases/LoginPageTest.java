3bd811966280a41977649ef222cbcfea9d241b78
PraveenRepo <praveen.intelegencia@gmail.com>
2018-06-07 09:04:46 +0530

    POM 1st commit

---
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,40 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+	
+	LoginPage loginPage;
+	
+	public LoginPageTest()
+	{
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		initlization();
+		loginPage = new LoginPage();
+	}
+	
+	@Test
+	public void loginPageTitleTest()
+	{
+		String title=loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Free CRM software in the cloud powers sales and customer service");
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
